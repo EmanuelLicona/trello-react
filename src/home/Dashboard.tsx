4797d6b000fa27973d2fc31588d48b74b4e5f6ef
EmanuelLicona <emanuel.licona02@gmail.com>
2024-01-17 23:12:35 -0600

    add custom input

---
+++ src/home/Dashboard.tsx
@@ -0,0 +1,23 @@
+
+export const Dashboard = () => {
+  return (
+    <div className='app'>
+
+      <div className='app-nav'>
+        <h1>Trello Kanban Board</h1>
+      </div>
+
+      <div className='app-boards-container'>
+        <div className='app-boards'>
+          {/* ! TODO create board component */}
+
+          <div className='app-boards-last'>
+          {/* ! TODO create Custom Input */}
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  )
+}
