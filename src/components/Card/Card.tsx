4fe59e20251ef7f552eba72f1cbd36fb45786c6f
EmanuelLicona <emanuel.licona02@gmail.com>
2024-01-18 22:59:36 -0600

    Update card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { CardProps } from '../../types'
 import { CardInfo } from '../CardInfo/CardInfo'
+import { AlignLeft, CheckSquare } from 'react-feather'
+import { formatDate } from '../../helpers/Utils'
 
 export const Card = (props: CardProps) => {
 
@@ -13,40 +15,66 @@ export const Card = (props: CardProps) => {
 
   return (
     <>
-        {
-          showModal && (
-            <CardInfo
-              card={card}
-              boardId={boardId}
-              onClose={() => setShowModal(false)}
-              updateCard={updateCard}
-            />
-          )
-        }
+      {
+        showModal && (
+          <CardInfo
+            card={card}
+            boardId={boardId}
+            onClose={() => setShowModal(false)}
+            updateCard={updateCard}
+          />
+        )
+      }
 
-      <div className='card'>
+      <div className='card' key={id}
+        onDragEnd={() => onDragEnd(boardId, id)}
+        onDragEnter={() => onDragEnter(boardId, id)}
+        onClick={() => setShowModal(true)}
+        draggable
+      >
         <div className='card-top'>
 
 
           <div className='card-top-labels'>
-
+            {
+              labels?.map((label, index) => (
+                // TODO label chip
+                <div key={index}>{JSON.stringify(label)}</div>
+              ))
+            }
           </div>
 
-          <div className='card-top-more'>
+          <div className='card-top-more'
+            onClick={(e) => {
+              e.stopPropagation()
+              setShowDropdown(true)
+            }}
+          >
             {/* TODO dropdown */}
           </div>
 
         </div>
 
 
-        <div className='card-title'>
-        </div>
+        <div className='card-title'>{title}</div>
 
         <div className='card-description'>
-
+          <p title={desc}><AlignLeft /></p>
         </div>
 
         <div className='card-footer'>
+          {date && (
+            <p className="card-footer-item">
+              {/* TODO date component */}
+              {formatDate(date)}
+            </p>
+          )}
+          {tasks && tasks?.length > 0 && (
+            <p className="card-footer-item">
+              <CheckSquare className="card-footer-icon" />
+              {tasks?.filter((item) => item.completed)?.length}/{tasks?.length}
+            </p>
+          )}
         </div>
 
 
